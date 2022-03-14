c2fceb55fe8c0bbf2b33c4353088c99f88f31731
gioitube <gioitube2k2@gmail.com>
2022-03-14 21:47:46 +0700

    deploy demo v_1.1

--- blog/views.py
+++ blog/views.py
@@ -103,12 +103,14 @@ def post_detail(request, year, month, day, post):
         new_comment.post = post
         new_comment.save()
 
-    comments_post = Comment.objects.filter(post=post).order_by('-created_on')
+    comments = Comment.objects.filter(post=post).order_by('-created_on')
+    comments_count = comments.count()
 
     return render(request, 'blog/blog-single.html',
                     {'domain': current_site,
                     'post': post,
                     'similar_posts': similar_posts,
                     'form': form,
-                    'comments_post': comments_post,
+                    'comments': comments,
+                    'comments_count': comments_count,
                     })
